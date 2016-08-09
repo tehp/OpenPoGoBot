bbe632e62cff30bf3a62fcff03549953c9379338
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2016-08-09 15:03:54 +0200

    manual transfer and evolve

--- plugins/socket/uievents.py
+++ plugins/socket/uievents.py
@@ -6,6 +6,12 @@ from plugins.socket import myjson
 
 # pylint: disable=unused-variable, unused-argument
 
+def find(f, seq):
+    for item in seq:
+        if f(item):
+            return item
+    return None
+
 def register_ui_events(socketio, state):
 
     @socketio.on("connect", namespace="/event")
@@ -62,12 +68,41 @@ def register_ui_events(socketio, state):
             }
             socketio.emit("eggs_list", emit_object, namespace="/event", room=request.sid)
 
+    @socketio.on("transfer_pokemon", namespace="/event")
+    def client_ask_for_transfer(evt):
+        if "bot" in state:
+            logger.log("Web UI action: Transfer", "yellow", fire_event=False)
+
+            bot = state["bot"]
+            pkm_id = int(evt["id"])
+
+            bot.api_wrapper.get_player().get_inventory()
+            all_pokemons = bot.api_wrapper.call()["pokemon"]
+
+            pokemon = find(lambda p: p.unique_id == pkm_id, all_pokemons)
+
+            if pokemon is not None:
+                bot.api_wrapper.release_pokemon(pokemon_id=int(evt["id"])).call()
+                bot.fire('after_transfer_pokemon', pokemon=pokemon)
+
     @socketio.on("evolve_pokemon", namespace="/event")
     def client_ask_for_evolve(evt):
         if "bot" in state:
-            # print evt
-            # print evt["id"]
             logger.log("Web UI action: Evolve", "yellow", fire_event=False)
+
             bot = state["bot"]
-            # bot.pokemon_list
-            # bot.api_wrapper.evolve_pokemon(pokemon_id=pokemon.unique_id).call()
+            pkm_id = int(evt["id"])
+
+            bot.api_wrapper.get_player().get_inventory()
+            all_pokemons = bot.api_wrapper.call()["pokemon"]
+
+            pokemon = find(lambda p: p.unique_id == pkm_id, all_pokemons)
+
+            if pokemon is not None:
+                bot.api_wrapper.evolve_pokemon(pokemon_id=int(evt["id"]))
+                response = bot.api_wrapper.call()
+                if response['evolution'].success:
+                    evolved_id = response['evolution'].get_pokemon().pokemon_id
+                    logger.log('Evolved {} into {}'.format(bot.pokemon_list[pokemon.pokemon_id - 1]['Name'], bot.pokemon_list[evolved_id - 1]['Name']))
+
+                    manager.fire_with_context('pokemon_evolved', bot, pokemon=pokemon, evolution=evolved_id)
