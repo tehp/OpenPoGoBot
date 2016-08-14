aacc2ffb3797e6353c13b5344fec0ee65584c9d7
Nicolas Schmitt <nicoschmitt@users.noreply.github.com>
2016-08-14 15:34:04 +0200

    UI - Logic to manually drop items (#299)
    * UI - Logic to manually drop items
    
    * use log function for consistency
    
    * use fire_event=False

--- plugins/socket/uievents.py
+++ plugins/socket/uievents.py
@@ -12,11 +12,14 @@ def find(f, seq):
             return item
     return None
 
+def log(text, color='yellow'):
+    logger.log(text, color=color, fire_event=False, prefix='UI')
+
 def register_ui_events(socketio, state):
 
     @socketio.on("connect", namespace="/event")
     def connect():
-        logger.log("Web client connected", "yellow", fire_event=False)
+        log("Web client connected")
         if "username" in state:
             emitted_object = state.copy()
             del emitted_object["bot"]
@@ -24,7 +27,7 @@ def register_ui_events(socketio, state):
 
     @socketio.on("disconnect", namespace="/event")
     def disconnect():
-        logger.log("Web client disconnected", "yellow", fire_event=False)
+        log("Web client disconnected")
 
     @socketio.on("pokemon_settings", namespace="/event")
     def client_ask_for_pokemon_settings():
@@ -39,7 +42,6 @@ def register_ui_events(socketio, state):
     @socketio.on("pokemon_list", namespace="/event")
     def client_ask_for_pokemon_list():
         if "bot" in state:
-            logger.log("Web UI action: Pokemon List", "yellow", fire_event=False)
             bot = state["bot"]
             bot.api_wrapper.get_player().get_inventory()
             inventory = bot.api_wrapper.call()
@@ -54,7 +56,6 @@ def register_ui_events(socketio, state):
     @socketio.on("inventory_list", namespace="/event")
     def client_ask_for_inventory_list():
         if "bot" in state:
-            logger.log("Web UI action: Inventory List", "yellow", fire_event=False)
             bot = state["bot"]
             bot.api_wrapper.get_player().get_inventory()
             inventory = bot.api_wrapper.call()
@@ -67,7 +68,6 @@ def register_ui_events(socketio, state):
     @socketio.on("player_stats", namespace="/event")
     def client_ask_for_player_stats():
         if "bot" in state:
-            logger.log("Web UI action: Player Stats", "yellow", fire_event=False)
             bot = state["bot"]
             bot.api_wrapper.get_player().get_inventory()
             inventory = bot.api_wrapper.call()
@@ -89,7 +89,6 @@ def register_ui_events(socketio, state):
     @socketio.on("eggs_list", namespace="/event")
     def client_ask_for_eggs_list():
         if "bot" in state:
-            logger.log("Web UI action: Eggs List", "yellow", fire_event=False)
             bot = state["bot"]
             bot.api_wrapper.get_player().get_inventory()
             inventory = bot.api_wrapper.call()
@@ -104,7 +103,7 @@ def register_ui_events(socketio, state):
     @socketio.on("transfer_pokemon", namespace="/event")
     def client_ask_for_transfer(evt):
         if "bot" in state:
-            logger.log("Web UI action: Transfer", "yellow", fire_event=False)
+            log("Web UI action: Transfer")
 
             bot = state["bot"]
             pkm_id = int(evt["id"])
@@ -122,15 +121,15 @@ def register_ui_events(socketio, state):
                 pokemon_name = bot.pokemon_list[pokemon_num - 1]["Name"]
                 pokemon_cp = pokemon.combat_power
                 pokemon_potential = pokemon.potential
-                logger.log("Transferring {0} (#{1}) with CP {2} and IV {3}".format(pokemon_name,
-                                                                                   pokemon_num,
-                                                                                   pokemon_cp,
-                                                                                   pokemon_potential))
+                log("Transferring {0} (#{1}) with CP {2} and IV {3}".format(pokemon_name,
+                                                                            pokemon_num,
+                                                                            pokemon_cp,
+                                                                            pokemon_potential), color="green")
 
     @socketio.on("evolve_pokemon", namespace="/event")
     def client_ask_for_evolve(evt):
         if "bot" in state:
-            logger.log("Web UI action: Evolve", "yellow", fire_event=False)
+            log("Web UI action: Evolve")
 
             bot = state["bot"]
             pkm_id = int(evt["id"])
@@ -145,6 +144,16 @@ def register_ui_events(socketio, state):
                 response = bot.api_wrapper.call()
                 if response['evolution'].success:
                     evolved_id = response['evolution'].get_pokemon().pokemon_id
-                    logger.log('Evolved {} into {}'.format(bot.pokemon_list[pokemon.pokemon_id - 1]['Name'], bot.pokemon_list[evolved_id - 1]['Name']))
+                    log('Evolved {} into {}'.format(bot.pokemon_list[pokemon.pokemon_id - 1]['Name'], bot.pokemon_list[evolved_id - 1]['Name']), color="green")
 
                     manager.fire_with_context('pokemon_evolved', bot, pokemon=pokemon, evolution=evolved_id)
+
+    @socketio.on("drop_items", namespace="/event")
+    def client_ask_for_item_drop(evt):
+        if "bot" in state:
+            bot = state["bot"]
+            item_id = int(evt["id"])
+            count = int(evt["count"])
+            item_name = bot.item_list[item_id]
+            log("Recycling {} {}{}".format(count, item_name, "s" if count > 1 else ""))
+            bot.api_wrapper.recycle_inventory_item(item_id=item_id, count=count).call()
